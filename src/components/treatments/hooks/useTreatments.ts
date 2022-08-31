17582001ab111c76fb2ee6903f857f1e52ccb50c
gesora96 <36685490+zahidhasanemran@users.noreply.github.com>
2022-08-31 22:38:50 +0600

    centralized error toast

--- src/components/treatments/hooks/useTreatments.ts
+++ src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -14,17 +13,9 @@ async function getTreatments(): Promise<Treatment[]> {
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
   const fallback = [];
-  const toast = useCustomToast();
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
     staleTime: 1000 * 10,
     retry: false,
-    onError: (error) => {
-      const title =
-        error instanceof Error
-          ? error.toString().replace(/^Error:\s*/, '')
-          : 'error connecting to the server';
-      toast({ title, status: 'error' });
-    },
   });
   return data;
 }
