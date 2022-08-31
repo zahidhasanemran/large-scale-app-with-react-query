2fb78048c36a13fb37ad74dc35c938fb5b5b3390
gesora96 <36685490+zahidhasanemran@users.noreply.github.com>
2022-08-31 22:25:38 +0600

    error handler for useQuery

--- src/components/treatments/hooks/useTreatments.ts
+++ src/components/treatments/hooks/useTreatments.ts
@@ -14,8 +14,17 @@ async function getTreatments(): Promise<Treatment[]> {
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
   const fallback = [];
+  const toast = useCustomToast();
   const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
     staleTime: 1000 * 10,
+    retry: false,
+    onError: (error) => {
+      const title =
+        error instanceof Error
+          ? error.toString().replace(/^Error:\s*/, '')
+          : 'error connecting to the server';
+      toast({ title, status: 'error' });
+    },
   });
   return data;
 }
