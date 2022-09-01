60088fda832f9cb41974ff34dd7549a9a9ca3a13
gesora96 <36685490+zahidhasanemran@users.noreply.github.com>
2022-09-01 15:06:00 +0600

    select for staff filter

--- src/components/staff/hooks/useStaff.ts
+++ src/components/staff/hooks/useStaff.ts
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
 import { useQuery } from 'react-query';
 
 import type { Staff } from '../../../../../shared/types';
@@ -22,8 +22,15 @@ export function useStaff(): UseStaff {
   // for filtering staff by treatment
   const [filter, setFilter] = useState('all');
 
+  const selectFn = useCallback(
+    (unfilteredStaff) => filterByTreatment(unfilteredStaff, filter),
+    [filter],
+  );
+
   // TODO: get data from server via useQuery
-  const { data = [] } = useQuery(queryKeys.staff, getStaff);
+  const { data = [] } = useQuery(queryKeys.staff, getStaff, {
+    select: filter !== 'all' ? selectFn : undefined,
+  });
 
   return { staff: data, filter, setFilter };
 }
